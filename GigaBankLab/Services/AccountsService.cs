246fd21762eb1465a4b2c4e09b71d187f74585cd
vshktd <dzmitry.vashkevich.a@gmail.com>
2024-03-11 01:41:11 +0300

    AccountService can create accounts for credit

--- GigaBankLab/Services/AccountsService.cs
+++ GigaBankLab/Services/AccountsService.cs
@@ -20,17 +20,41 @@ namespace GigaBankLab.Services
             var accountId = await GetAccountsNumber(depositContractDTO.ClientId);
             var current = new Account()
             {
-                Number = Account.GenerateNumber(AccountConstants.CurrentAccountCode, depositContractDTO.ClientId, accountId),
+                Number = Account.GenerateNumber(AccountCodes.CurrentAccountCode, depositContractDTO.ClientId, accountId),
                 ClientId = depositContractDTO.ClientId,
                 CurrencyId = deposit.CurrencyId,
                 Type = AccountType.Passive
             };
             var percent = new Account()
             {
-                Number = Account.GenerateNumber(AccountConstants.PercentAccountCode, depositContractDTO.ClientId, accountId),
+                Number = Account.GenerateNumber(AccountCodes.PercentAccountCode, depositContractDTO.ClientId, accountId),
                 ClientId = depositContractDTO.ClientId,
                 CurrencyId = deposit.CurrencyId,
                 Type = AccountType.Passive
+            };
+
+            await context.AddRangeAsync(current, percent);
+            await context.SaveChangesAsync();
+
+            return (current, percent);
+        }
+
+        public async Task<(Account current, Account percent)> CreateCreditAccounts(Credit credit, CreditContractDTO creditContractDTO)
+        {
+            var accountId = await GetAccountsNumber(creditContractDTO.ClientId);
+            var current = new Account()
+            {
+                Number = Account.GenerateNumber(AccountCodes.CreditAccountCode, creditContractDTO.ClientId, accountId),
+                ClientId = creditContractDTO.ClientId,
+                CurrencyId = credit.CurrencyId,
+                Type = AccountType.Active
+            };
+            var percent = new Account()
+            {
+                Number = Account.GenerateNumber(AccountCodes.CreditPercentCode, creditContractDTO.ClientId, accountId),
+                ClientId = creditContractDTO.ClientId,
+                CurrencyId = credit.CurrencyId,
+                Type = AccountType.Active
             };
 
             await context.AddRangeAsync(current, percent);
